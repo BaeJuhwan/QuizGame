24cb3286799d94645913de6432fc269d3ef2d621
BaeJuhwan <bjw505@naver.com>
2021-12-29 22:29:20 +0900

    first commit

---
+++ QuizGame/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace QuizGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tName = new System.Windows.Forms.TextBox();
+            this.btn001 = new System.Windows.Forms.Button();
+            this.btn002 = new System.Windows.Forms.Button();
+            this.btn003 = new System.Windows.Forms.Button();
+            this.btn004 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tName
+            // 
+            this.tName.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tName.Location = new System.Drawing.Point(0, 0);
+            this.tName.Multiline = true;
+            this.tName.Name = "tName";
+            this.tName.ReadOnly = true;
+            this.tName.Size = new System.Drawing.Size(800, 129);
+            this.tName.TabIndex = 0;
+            // 
+            // btn001
+            // 
+            this.btn001.Location = new System.Drawing.Point(12, 178);
+            this.btn001.Name = "btn001";
+            this.btn001.Size = new System.Drawing.Size(248, 42);
+            this.btn001.TabIndex = 1;
+            this.btn001.Text = "button1";
+            this.btn001.UseVisualStyleBackColor = true;
+            this.btn001.Click += new System.EventHandler(this.btn001_Click);
+            // 
+            // btn002
+            // 
+            this.btn002.Location = new System.Drawing.Point(12, 226);
+            this.btn002.Name = "btn002";
+            this.btn002.Size = new System.Drawing.Size(248, 45);
+            this.btn002.TabIndex = 2;
+            this.btn002.Text = "button2";
+            this.btn002.UseVisualStyleBackColor = true;
+            this.btn002.Click += new System.EventHandler(this.btn002_Click);
+            // 
+            // btn003
+            // 
+            this.btn003.Location = new System.Drawing.Point(12, 277);
+            this.btn003.Name = "btn003";
+            this.btn003.Size = new System.Drawing.Size(248, 44);
+            this.btn003.TabIndex = 3;
+            this.btn003.Text = "button3";
+            this.btn003.UseVisualStyleBackColor = true;
+            this.btn003.Click += new System.EventHandler(this.btn003_Click);
+            // 
+            // btn004
+            // 
+            this.btn004.Location = new System.Drawing.Point(12, 327);
+            this.btn004.Name = "btn004";
+            this.btn004.Size = new System.Drawing.Size(248, 39);
+            this.btn004.TabIndex = 4;
+            this.btn004.Text = "button4";
+            this.btn004.UseVisualStyleBackColor = true;
+            this.btn004.Click += new System.EventHandler(this.btn004_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn004);
+            this.Controls.Add(this.btn003);
+            this.Controls.Add(this.btn002);
+            this.Controls.Add(this.btn001);
+            this.Controls.Add(this.tName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tName;
+        private System.Windows.Forms.Button btn001;
+        private System.Windows.Forms.Button btn002;
+        private System.Windows.Forms.Button btn003;
+        private System.Windows.Forms.Button btn004;
+    }
+}
+
---
+++ QuizGame/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuizGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public List<string> theQuestions;
+        public List<string> theSelect1;
+        public List<string> theSelect2;
+        public List<string> theSelect3;
+        public List<string> theSelect4;
+        public List<int> theAnswer;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            theQuestions = new List<string>();
+            theSelect1 = new List<string>();
+            theSelect2 = new List<string>();
+            theSelect3 = new List<string>();
+            theSelect4 = new List<string>();
+            theAnswer = new List<int>();
+
+            theQuestions.Add("다음 중 표준어가 아닌것은?");
+            theSelect1.Add("윗목");
+            theSelect2.Add("윗돈");
+            theSelect3.Add("위층");
+            theSelect4.Add("웃옷");
+            theAnswer.Add(2);
+
+            theQuestions.Add("옥수수-강냉이");
+            theSelect1.Add("친구-벗");
+            theSelect2.Add("보조개-볼우물");
+            theSelect3.Add("매니저-관리인");
+            theSelect4.Add("소금-염화나트륨");
+            theAnswer.Add(2);
+
+            theQuestions.Add("다음 중 표준어인 것은?");
+            theSelect1.Add("담쟁이덩쿨");
+            theSelect2.Add("벌러지");
+            theSelect3.Add("푸줏관");
+            theSelect4.Add("고까옷");
+            theAnswer.Add(4);
+
+            theQuestions.Add("표기와 발음이 옳지 않는 것은?");
+            theSelect1.Add("웃옷[우돋]");
+            theSelect2.Add("윗잇몸[위딛몸]");
+            theSelect3.Add("윗변[윋뼌]");
+            theSelect4.Add("웃돈[욷똔]");
+            theAnswer.Add(2);
+
+            tName.Text = theQuestions[0];
+            btn001.Text = theSelect1[0];
+            btn002.Text = theSelect2[0];
+            btn003.Text = theSelect3[0];
+            btn004.Text = theSelect4[0];
+
+            theCurrentIndex = 0;
+            theScore = 0;
+        }
+        int theCurrentIndex;
+        int theScore;
+        private void btn001_Click(object sender, EventArgs e)
+        {
+            if(theCurrentIndex >= theQuestions.Count)
+            {
+                return;
+            }
+            if(theAnswer[theCurrentIndex] == 1)
+            {
+                theScore += 100;
+            }
+            theCurrentIndex++;
+            if(theCurrentIndex >= theQuestions.Count)
+            {
+                tName.Text = String.Format("모든 문제를 다 풀었습니다. {0}점", theScore);
+            }
+            else
+            {
+                tName.Text = theQuestions[theCurrentIndex];
+                btn001.Text = theSelect1[theCurrentIndex];
+                btn002.Text = theSelect2[theCurrentIndex];
+                btn003.Text = theSelect3[theCurrentIndex];
+                btn004.Text = theSelect4[theCurrentIndex];
+
+            }
+        }
+        private void ViewQusetion(int aSelect)
+        {
+            if(theAnswer[theCurrentIndex] == aSelect)
+            {
+                theScore += 100;
+            }
+            theCurrentIndex++;
+            if (theCurrentIndex >= theQuestions.Count)
+            {
+                tName.Text = String.Format("모든 문제를 다 풀었습니다.{0}점", theScore);
+            }
+            else
+            {
+                tName.Text = theQuestions[theCurrentIndex];
+                btn001.Text = theSelect1[theCurrentIndex];
+                btn002.Text = theSelect2[theCurrentIndex];
+                btn003.Text = theSelect3[theCurrentIndex];
+                btn004.Text = theSelect4[theCurrentIndex];
+
+            }
+        }
+        private void btn002_Click(object sender, EventArgs e)
+        {
+            if(theCurrentIndex >= theQuestions.Count)
+            {
+                return;
+            }
+            ViewQusetion(2);
+        }
+
+        private void btn003_Click(object sender, EventArgs e)
+        {
+            if (theCurrentIndex >= theQuestions.Count)
+            {
+                return;
+            }
+            ViewQusetion(3);
+        }
+
+        private void btn004_Click(object sender, EventArgs e)
+        {
+            if (theCurrentIndex >= theQuestions.Count)
+            {
+                return;
+            }
+            ViewQusetion(4);
+        }
+    }
+}
